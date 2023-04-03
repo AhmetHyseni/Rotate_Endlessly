74b8b41d2ff7332b191c06e03ad491787651317c
AhmetHyseni <ahmetfin4b@gmail.com>
2023-04-03 11:13:55 +0300

    Track spawner and movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rb;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove);
+    }
+    private void Update()
+    {
+        
+    }
+}
